143a99cb5e73172d30fa3b3408c6245ff547afc2
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-07-27 16:47:35 +0530

    contract deployed locally

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9 ;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+
+    constructor() {
+        console.log("Yo yo, I'm writing a smart-contract for first time :D");
+    }
+
+}
